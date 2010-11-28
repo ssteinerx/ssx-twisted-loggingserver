1d1b5326df0c8dff7d630e45225625c6e5eed53c
Steve Steiner <ssteinerX@gmail.com>
2010-11-27 19:55:41 -0500

    Trying to get setuptools to install plugin where it can be found

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ setup(name='loggingserver',
       zip_safe=False,
       packages=[
          "loggingserver",
+         "twisted.plugins",
       ],
       package_data = {
          # If any package contains *.txt or *.rst files, include them:
          '': ['*.conf', '*.rst'],
-         'twisted_plugin': ['twisted'],
+         'twisted' : ['plugins/loggingplugin.py',],
       },
       author=__author__,
       author_email="ssteinerX@gmail.com",
-      url="http://https://github.com/ssteinerx/ssx-twisted-loggingserver",
+      url="http://github.com/ssteinerx/ssx-twisted-loggingserver",
 )
